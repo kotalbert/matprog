d86f6022ae7521ffaf02b6b0aa949d6c00c1b3cb
Pawel Daniluk <pawel@daniluk.waw.pl>
2017-02-10 19:45:49 +0100

    homework 3 completed

--- hw3/accelerate.m
+++ hw3/accelerate.m
@@ -1,4 +1,4 @@
 function amag = accelerate(F1, F2, m)
-a = (F1(1) + F2(1))^2 + (F1(2) + F2(2))^2 + (F1(3) + F2(3))^2;
-a = sqrt(a);
+c = F1 + F2;
+a = sqrt(c'*c);
 amag = a*m;
--- hw3/light_speed.m
+++ hw3/light_speed.m
@@ -1,6 +1,6 @@
 function [times, miles] = light_speed(distances)
 c=3e5;
-mi=1.607;
+mi=1.609;
 
 times = (distances/c)/60;
-miles = distances*mi;
+miles = distances/mi;
