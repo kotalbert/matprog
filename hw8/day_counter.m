046b3672749b2ac957aa7b13c240ea2b18bf8515
Pawel Daniluk <pawel@daniluk.waw.pl>
2017-06-05 20:43:41 +0200

    day_counter complete

--- hw8/day_counter.m
+++ hw8/day_counter.m
@@ -1,27 +1,11 @@
-function  dc =  day_counter(yr)
-% 1 Jan 1776: Monday
-yz = 1776;
+function a_int = day_counter(yr)
+% Calculate number of Mondays on 1st of each month in year yr
 
-% Calculate if 'yr' is leap year
-lyn = leap_year_number(yr);
+% 1776-01-01 -> Monday
+global start_yr; % reference date
+global ndays; % number of days in months of year yr
 
-% Check what day is 1st Jan in 'yr'
-ydif = yr - yz;
-% if yr == 1900 || yr == 2015 || yr == 1898 || yr == 1855 || yr == 1877 || yr == 1937
-%     dn = 365*ydif + lyn;
-% else
-%    
-% end
- dn = 365*ydif + lyn - 1;
-% 1st Jan 1776 is Monday, 
-% modulo to show which day is 1st Jan in a given yaer
-mon1st = mod(dn,7);
-
-if mon1st == 0
-    dc = 1;
-else
-    dc = 0;
-end
+start_yr = 1776;
 
 if is_leap_year(yr)
     febdays = 29;
@@ -32,33 +16,69 @@ end
 % jan feb mar apr may jun jul aug sep oct nov dec
 ndays = [31 febdays 31 30 31 30 31 31 30 31 30 31];
 
-% Monve to first of the consecutive months
+a_int = count_mondays(yr);
 
-for m = 1:12
-    mon1st = mon1st + ndays(m);
-    if mod(mon1st, 7) == 0
-        dc = dc + 1;
-    end;
 end
 
-% Count number of Mondays
-
-function flag = is_leap_year(yr)
-    if mod(yr, 400) == 0
-        flag = 1;
-    elseif mod(yr,4)==0 && mod(yr,100)~=0
-        flag = 1;
+function a_log = is_monday(day_number)
+% calulate if day_number (number of days from start date) is Monday
+    if mod(day_number, 7) == 0
+        a_log = 1;
     else
-        flag = 0;
+        a_log = 0;
     end
-
-function lyn = leap_year_number(yr)
-yz = 1776;
-lyn = 0;
-
-for y = yz:yr
-    if is_leap_year(y)
-        lyn = lyn + 1;
-    end;
 end
-        
+
+function a_log = is_leap_year(this_yr)
+% check if this_yr is a leap year
+    if mod(this_yr, 400) == 0
+        a_log = 1;
+    elseif mod(this_yr,4)==0 && mod(this_yr,100)~=0
+        a_log = 1;
+    else
+        a_log = 0;
+    end
+end
+
+function a_int = get_number_leap_years(yr)
+% Calculate, how many leap years passed between start and yr
+global start_yr;
+lyn = 0; % leap year number
+    for i = start_yr : yr - 1
+        if is_leap_year(i)
+            lyn = lyn + 1;
+        end
+    end
+a_int = lyn;
+end
+
+
+function a_int = get_yr_first_day(yr)
+% calulate what day is the first day of year of yr
+global start_yr;
+    a_int = (yr - start_yr) * 365 + get_number_leap_years(yr);
+end
+
+function a_int = count_mondays(yr)
+% count Mondays, on first day of each month in a year yr
+global ndays;
+
+yr_first_day = get_yr_first_day(yr);
+
+if is_monday(yr_first_day)
+    mon_num = 1;
+else
+    mon_num = 0;
+end
+m_first_day = yr_first_day;
+for m = 1:11
+    m_first_day = m_first_day + ndays(m);
+    if is_monday(m_first_day)
+        mon_num = mon_num + 1;
+    end
+end
+
+a_int = mon_num;
+
+end
+
